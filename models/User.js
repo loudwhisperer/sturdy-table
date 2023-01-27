bef5d24af6cb879c418709bce9aaf2b215502da2
loudwhisperer <driftaway44@gmail.com>
2023-01-26 21:16:50 -0700

    added event.js and user.js

--- models/User.js
+++ models/User.js
@@ -3,71 +3,74 @@ const sequelize = require("../config/connection");
 const bcrypt = require("bcrypt");
 
 class User extends Model {
-    checkPassword(loginPw) {
-        return bcrypt.compareSync(loginPw, this.password);
-    }
+  checkPassword(loginPw) {
+    return bcrypt.compareSync(loginPw, this.password);
+  }
 }
 
 User.init(
-    {
-        id: {
-            type: DataTypes.INTEGER, 
-            allowNull: false, 
-            primaryKey: true, 
-            autoIncrement: true,
-        }, 
-        email: {
-            type: DataTypes.STRING, 
-            validate: {
-                isEmail: true,
-            },
-            allowNull: false,
-        },
-        password: {
-            type: DataTypes.STRING, 
-            allowNull: false, 
-            validate: {
-                len: [8],
-                is: "(?=^.{8,}$)((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$", 
-            },
-        },
-        displayname: {
-            type: DataTypes.STRING, 
-            allowNull: false, 
-            validate: {
-                isAlphanumeric: true, 
-                len: [3, 30],
-            },
-       },
-       avatarpic: {
-        type: DataTypes.BLOB, 
-        allowNull: true, 
-       },
-       friends_list: {
-        type: DataTypes.STRING, 
-        allowNull: true, 
-        references: {
-            model: "user",
-            key: "id",
-            unique: true,
-            },
-        },
-    }, 
-    {
-        hooks: {
-            beforeCreate: async (newUserData) => {
-                newUserData.password = await bcrypt.hash(newUserData.password, 10);
-                return newUserData; 
-            }, 
-            beforeUpdate: async (updatedUserData) => {
-                updatedUserData.password = await bcrypt.hash(updatedUserData.password, 10);
-                return updatedUserData;
-            },
-        }, 
-        sequelize,
-        timestamps: false,
-        freezeTableName: true,
-        underscored: true,
-        modelName: 'user',
-    }
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    email: {
+      type: DataTypes.STRING,
+      validate: {
+        isEmail: true,
+      },
+      allowNull: false,
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [8],
+        is: "(?=^.{8,}$)((?=.*d)|(?=.*W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$",
+      },
+    },
+    displayname: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        isAlphanumeric: true,
+        len: [3, 30],
+      },
+    },
+    avatarpic: {
+      type: DataTypes.BLOB,
+      allowNull: true,
+    },
+    friends_list: {
+      type: DataTypes.STRING,
+      allowNull: true,
+      references: {
+        model: "user",
+        key: "id",
+        unique: true,
+      },
+    },
+  },
+  {
+    hooks: {
+      beforeCreate: async (newUserData) => {
+        newUserData.password = await bcrypt.hash(newUserData.password, 10);
+        return newUserData;
+      },
+      beforeUpdate: async (updatedUserData) => {
+        updatedUserData.password = await bcrypt.hash(
+          updatedUserData.password,
+          10
+        );
+        return updatedUserData;
+      },
+    },
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: "user",
+  }
 );
