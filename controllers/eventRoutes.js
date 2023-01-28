2b978bde3c4b5d9f54a3bcc8dc16f73e63123277
Christin Carter <raspberry.muppet@gmail.com>
2023-01-28 16:58:25 -0700

    established models for friend requests

--- controllers/eventRoutes.js
+++ controllers/eventRoutes.js
@@ -1,5 +1,5 @@
 const router = require("express").Router();
-const { User, Event } = require("../models");
+const { User, Event, Friend, EventRoutes } = require("../models");
 
 router.get("/", async (req, res) => {
   try {
