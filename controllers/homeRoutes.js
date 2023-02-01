6ff0c5781223639e071e294cb217f5a8c6d6eff2
Joshua Taylor <toasterrage@gmail.com>
2023-02-01 15:02:04 -0700

    Added homeRoutes with basic code

---
+++ controllers/homeRoutes.js
@@ -0,0 +1,15 @@
+// Import modules
+const router = require('express').Router();
+const { Event } = require('../models');
+
+// GET for homepage
+router.get('/', async (req, res) => {
+  try {
+
+  } 
+  catch(err) {
+
+  }
+});
+
+module.exports = router;
