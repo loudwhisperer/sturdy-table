f63ce5221afec18a8e8a0567ad5c4619ca61ffad
Joshua Taylor <toasterrage@gmail.com>
2023-01-31 19:52:33 -0700

    Added gmail-send npm package and local only temp file to hold security details

---
+++ utils/send-email.js
@@ -0,0 +1,10 @@
+// This will need to be moved to a more secure location
+
+//gmail app password
+
+const send = require('gmail-send') ({
+  user: 'project.thetable@gmail.com',
+  pass: 'ezduqdcwlijwqpvq',
+  to: '',
+  subject: ''
+})
