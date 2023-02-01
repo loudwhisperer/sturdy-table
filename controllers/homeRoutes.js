40fb9a5f8857caf65158491539fbfc7875626ded
Joshua Taylor <toasterrage@gmail.com>
2023-02-01 15:11:11 -0700

    Updated homeRoutes with db query and send handlebars

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -5,10 +5,19 @@ const { Event } = require('../models');
 // GET for homepage
 router.get('/', async (req, res) => {
   try {
-
+    // Get event db data
+    const eventData = await Event.findAll({
+      // Use the below code to add columns from related tables
+      // include: [{ model: Table, attributes: ['column name'] }]
+    });
+    const events = eventData.map((event) => event.get({ plain:true }));
+    console.info(events);
+    // Send handlebars page to user
+    res.render('homepage', { events });
   } 
   catch(err) {
-
+    console.log(err);
+    res.status(500).json(err);
   }
 });
 
