60741ce44de0687f007cda9b67d90faeee5086df
artiecannv <thefiftharthur@gmail.com>
2023-02-03 19:15:04 -0700

    making sure im up to date

---
+++ controllers/api/accountRoutes.js
@@ -0,0 +1,8 @@
+const router = require("express").Router();
+const { User, Event } = require('../../models');
+
+router.get("/", async (req, res) => {
+    try {
+        const accountData = await 
+    }
+})
