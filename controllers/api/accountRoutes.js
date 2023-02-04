7c769f7386d4be31cffd7d578ea931427683b48e
artiecannv <thefiftharthur@gmail.com>
2023-02-03 21:27:55 -0700

    added the account.get into the userRoutes

--- controllers/api/accountRoutes.js
+++
@@ -1,8 +0,0 @@
-const router = require("express").Router();
-const { User, Event } = require('../../models');
-
-router.get("/", async (req, res) => {
-    try {
-        const accountData = await 
-    }
-})
