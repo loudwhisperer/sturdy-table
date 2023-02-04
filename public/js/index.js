edeedf27115f5449333ad8671f7716803328e000
Joshua Taylor <toasterrage@gmail.com>
2023-02-04 13:37:48 -0700

    Got full event page working

--- public/js/index.js
+++ public/js/index.js
@@ -1,13 +1,6 @@
 // *****Code to send user to full event page from homepage *****
 const getFullEvent = async (eventId) => {
   // Request full event page from backend
-  const response = await fetch(`/api/events/${eventId}`, {
-    method: 'GET',
-    //body: '',
-    headers: { 'content-type': 'application/json' }
-  });
-
-  if (response.ok) {
-    console.log(response);
-  }
+  const currentUrl = document.location.href;
+  document.location.href = currentUrl + `api/events/${eventId}`;
 }
