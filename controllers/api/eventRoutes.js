4a1352daaa97e8597017e41ccef34b3381467b8a
artiecannv <thefiftharthur@gmail.com>
2023-02-04 21:02:20 -0700

    successfully added helper script to homepage and event pages

--- controllers/api/eventRoutes.js
+++ controllers/api/eventRoutes.js
@@ -1,9 +1,9 @@
 const router = require("express").Router();
 const { User, Event, Friend, Eventgroup } = require("../../models");
 
-//frontend route that is expecting a res.render of a handlebars component 
+//frontend route that is expecting a res.render of a handlebars component
 
-//get al events
+//get all events
 router.get("/", async (req, res) => {
   try {
     const eventData = await Event.findAll({
@@ -17,12 +17,12 @@ router.get("/", async (req, res) => {
 
 // get an event with all fields and all users in the event
 router.get("/:id", async (req, res) => {
-    try{
+  try {
     const eventData = await Event.findByPk(req.params.id, {
       include: [
         {
           model: User,
-          as: 'party_members'
+          as: "party_members",
         },
       ],
       // TODO: add an attributes to have everything returned in the user object but the encrypted password
@@ -43,56 +43,56 @@ router.get("/:id", async (req, res) => {
     });
 
     //res.status(200).json(eventData);
-} catch(err){
-  res.status(500).json(err.message);
-}
+  } catch (err) {
+    res.status(500).json(err.message);
+  }
 });
 
-// create an event 
-router.post("/", async (req, res) =>{
-  try{
-    const dbEventData = await Event.create(
-      req.body
-    );
+// create an event
+router.post("/", async (req, res) => {
+  try {
+    const dbEventData = await Event.create(req.body);
     res.status(200).json(dbEventData);
-  } catch(err){
+  } catch (err) {
     console.log(err);
     res.status(500).json(err);
   }
-})
+});
 
-//create a user in an event 
+//create a user in an event
 router.post("/attending", async (req, res) => {
-  try{
-  const data = await Eventgroup.create(req.body)
-  res.status(200).json(data);
-  } catch(err){
+  try {
+    const data = await Eventgroup.create(req.body);
+    res.status(200).json(data);
+  } catch (err) {
     res.status(500).json(err.message);
   }
-})
+});
 
-//get a user in an event 
+//get a user in an event
 router.get("/attending/:id", async (req, res) => {
-  try{
-  const eventData = await Eventgroup.findAll({where:{eventId: req.params.id} })
-  res.status(200).json(eventData);
-  } catch(err){
+  try {
+    const eventData = await Eventgroup.findAll({
+      where: { eventId: req.params.id },
+    });
+    res.status(200).json(eventData);
+  } catch (err) {
     res.status(500).json(err.message);
   }
-})
+});
 
 //update an event and the users in it
 router.put("/:id", async (req, res) => {
-  try{
-  const data = await Event.update(req.body, {
-    where: {
-      id: req.params.id,
-    },
-  })
-  res.status(200).json(data);
-} catch(err){
-  res.status(500).json(err);
-}
+  try {
+    const data = await Event.update(req.body, {
+      where: {
+        id: req.params.id,
+      },
+    });
+    res.status(200).json(data);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 //delete an event by id
@@ -103,10 +103,10 @@ router.delete("/:id", (req, res) => {
 });
 
 //delete users from an event
-router.delete("/attending/:id", (req,res) => {
-  Eventgroup.destroy({where:{eventId: req.params.id}}).then((data) =>
+router.delete("/attending/:id", (req, res) => {
+  Eventgroup.destroy({ where: { eventId: req.params.id } }).then((data) =>
     res.json(data)
-  )
+  );
 });
 
 module.exports = router;
